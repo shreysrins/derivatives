c70dd77f70236801c7c8be77ac533e05b598384e
Shreyas Srinivasan <shreyass@mit.edu>
2022-02-06 03:07:58 -0500

    Add initial package documentation

---
+++ derivatives/__init__.py
@@ -0,0 +1,6 @@
+"""
+Financial Derivatives analytics
+
+This package was developed for 15.437 Options and Futures Markets, as taught by Professor Deborah J. Lucas at the MIT Sloan School of Management. Its recommended usage is as
+    >>> import derivatives as derivs
+"""
